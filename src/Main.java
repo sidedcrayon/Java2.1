2110e8b9c007193a3c6b7d433b946a1561773ce7
Ksenia Starchenko <starchenko.ksenia@gmail.com>
2023-02-20 18:41:00 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main (String[] args) {
+
+        int price = 31000;
+        int miles = 20;
+
+        int bonus;
+        {
+            bonus = price / miles;
+        }
+
+System.out.println("Количество бонусов =");
+        System.out.println(bonus);
+
+    }
+
+}
